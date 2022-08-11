15d5e7b1ac2bdb64300fd244fccc208af11ab973
AnsonLoHK <chiaochiao1106@gmail.com>
2022-08-11 11:54:53 +0800

    read file 正常 獲取 data get method 接下來要移植去 獨立api裡 (api.js

--- src/components/Read.jsx
+++ src/components/Read.jsx
@@ -1,26 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Table } from "semantic-ui-react";
+import axios from "axios";
 
 const Read = () => {
+  const [APIData, setAPIData] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`https://62f1f364b1098f150807dadd.mockapi.io/fakeData`)
+      .then((response) => {
+        setAPIData(response.data);
+      });
+  }, []);
+
+  console.log("APIData", APIData);
   return (
     <div>
       <Table singleLine>
         <Table.Header>
           <Table.Row>
-            <Table.HeaderCell>Name</Table.HeaderCell>
-            <Table.HeaderCell>Registration Date</Table.HeaderCell>
-            <Table.HeaderCell>E-mail address</Table.HeaderCell>
-            <Table.HeaderCell>Premium Plan</Table.HeaderCell>
+            <Table.HeaderCell>First Name</Table.HeaderCell>
+            <Table.HeaderCell>Last Name</Table.HeaderCell>
+            <Table.HeaderCell>Checked</Table.HeaderCell>
           </Table.Row>
         </Table.Header>
 
         <Table.Body>
-          <Table.Row>
-            <Table.Cell>John Lilki</Table.Cell>
-            <Table.Cell>September 14, 2013</Table.Cell>
-            <Table.Cell>jhlilk22@yahoo.com</Table.Cell>
-            <Table.Cell>No</Table.Cell>
-          </Table.Row>
+          {APIData.map((data) => {
+            return (
+              <Table.Row>
+                <Table.Cell>{data.firstName}</Table.Cell>
+                <Table.Cell>{data.lastName}</Table.Cell>
+                <Table.Cell>
+                  {data.checkbox ? "Checked" : "Unchecked"}
+                </Table.Cell>
+              </Table.Row>
+            );
+          })}
         </Table.Body>
       </Table>
     </div>
